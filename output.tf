9b06d6574f6b172955e1b409607d324e4521cece
Surekha <surekhagidithuri@gmail.com>
2025-06-25 12:15:12 +0530

    export security group IDs

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "sg_id" {
-  value = aws_security_group.main.id # get the security group id
+output "sg_id" {                     # we use this sg_id to refer this output
+  value = aws_security_group.main.id # get the security group id from the respective resource
 }
