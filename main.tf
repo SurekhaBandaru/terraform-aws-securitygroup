4abe4b3ed0011be77e50c47a07f48e7d9011d041
Surekha <surekhagidithuri@gmail.com>
2025-06-28 15:52:26 +0530

    egress rule

--- main.tf
+++ main.tf
@@ -3,11 +3,21 @@ resource "aws_security_group" "main" { # security group id inside attached vpc (
   description = var.sg_description
   vpc_id      = var.vpc_id
 
+  #allow traffic to outside
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = merge(var.sg_tags,
     local.common_tags,
     {
       Name = "${var.project}-${var.environment}-${var.sg_name}" #roboshop-dev-frontend
   })
+
+
 
 }
