0fd3b99b6dc93bb910dbbf107c2614e64fbc43dd
Surekha <surekhagidithuri@gmail.com>
2025-07-08 23:33:10 +0530

    sg

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "main" { # security group id inside attached vpc (vpic id)
-  name        = var.sg_name
+  name        = "${var.project}-${var.environment}-${var.sg_name}"
   description = var.sg_description
   vpc_id      = var.vpc_id
 
